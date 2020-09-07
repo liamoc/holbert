e46f040e781407da330e62ee6ea861df9c2586c7
Liam O’Connor <liamoc@cse.unsw.edu.au>
2020-09-07 20:23:59 +1000

    Great refactoring: ran a formatter

--- View/ProofTree.hs
+++ View/ProofTree.hs
@@ -20,45 +20,52 @@ renderProofTree :: DisplayOptions -> Int -> ProofTree -> Maybe (R.Focus R.Rule, 
 renderProofTree opts idx pt selected = renderPT [] [] [] pt
   where
     termDOs = tDOs opts
-    ruleDOs = RDO { termDisplayOptions = termDOs , showEmptyTurnstile = False, showInitialMetas = True, ruleStyle = Turnstile }
+    ruleDOs = RDO {termDisplayOptions = termDOs, showEmptyTurnstile = False, showInitialMetas = True, ruleStyle = Turnstile}
 
     renderPT :: [Prop] -> [String] -> ProofTree.Path -> ProofTree -> View EditorAction
-    renderPT rns ctx pth (PT sks lcls prp msgs) = let
-           binders = (if (showMetaBinders opts) then concat (zipWith (metabinder' pth) [0..] sks) else []) 
-                  ++ (if (assumptionsMode opts == Hidden) then map rulebinder [length rns..length rns+length lcls - 1] else []) 
-           premises = case msgs of  
-                     Just (rr,sgs) -> zipWith (renderPT rns' ctx') (map (:pth) [0..]) sgs
-                     Nothing       -> []
-           spacer = case msgs of 
-                     Just _ -> text "" 
-                     _ -> goalButton pth
-           ruleTitle = maybe (text "?") (addNix . renderRR . fst) msgs
-           conclusionTerm = renderTermCtx ctx' termDOs prp
-           conclusion = case assumptionsMode opts of 
-                        New | not (null lcls) -> [numberedAssumptions [length rns..] lcls , turnstile, conclusionTerm]
-                        Cumulative            -> [numberedAssumptions [0..]          rns' , turnstile, conclusionTerm]
-                        _                     -> [conclusionTerm]
-        in inferrule binders premises spacer ruleTitle conclusion
+    renderPT rns ctx pth (PT sks lcls prp msgs) =
+      let binders = (if showMetaBinders opts then concat (zipWith (metabinder' pth) [0 ..] sks) else [])
+                 ++ (if assumptionsMode opts == Hidden then map rulebinder [length rns .. length rns + length lcls - 1] else [])
+          premises = case msgs of
+            Just (rr, sgs) -> zipWith (renderPT rns' ctx') (map (: pth) [0 ..]) sgs
+            Nothing        -> []
+          spacer = maybe (goalButton pth) (const $ text "") msgs
+
+          ruleTitle = maybe (text "?") (addNix . renderRR . fst) msgs
+
+          conclusionTerm = renderTermCtx ctx' termDOs prp
+
+          conclusion = case assumptionsMode opts of
+            New | not (null lcls) -> [numberedAssumptions [length rns ..] lcls, turnstile, conclusionTerm]
+            Cumulative            -> [numberedAssumptions [0 ..] rns', turnstile, conclusionTerm]
+            _                     -> [conclusionTerm]
+       in inferrule binders premises spacer ruleTitle conclusion
+
       where
-        addNix t = multi [t, button "button-icon button-icon-red" (ItemAction (Just idx) $ I.RuleAct $ R.Nix pth) [typicon "trash"] ]
+        addNix t = multi [t, button "button-icon button-icon-red" (ItemAction (Just idx) $ I.RuleAct $ R.Nix pth) [typicon "trash"]]
+
         rulebinder v = multi [localrule v, miniTurnstile]
+
         rns' = map (Prop.raise (length sks)) rns ++ lcls
         ctx' = reverse sks ++ ctx
+
         numberedAssumptions numbers assumptions = context (intersperse comma $ zipWith renderPropLabelled numbers assumptions)
         renderPropLabelled i p = labelledBrackets (renderProp ctx' ruleDOs p) (localrule i)
-        
-    metabinder' pth i n = case selected of
-                             Just (R.ProofBinderFocus pth' i', n) | pth == pth', i == i' -> [metabinderEditor pth i n] 
-                             _ -> [button "editable editable-math" (SetFocus $ ItemFocus idx $ I.RuleFocus $ R.ProofBinderFocus pth i) [ metabinder n ]]
-    metabinderEditor pth i n = form_ [ class_ "editor editor-expanding", onSubmit (ItemAction (Just idx) $ I.RuleAct $ R.RenameProofBinder pth i) ] 
-                                     [ expandingTextbox "editor-textbox" UpdateInput n
-                                     , submitButton "button-icon button-icon-blue" [typicon "tick-outline"]
-                                     , button "button-icon button-icon-grey" Reset [typicon "times-outline"] 
-                                     , focusHack "editor-textbox"
-                                     ]
 
-    
-    goalButton pth = if Just (R.GoalFocus pth) == fmap fst selected then 
-                        focusedButton "button-icon button-icon-active button-icon-goal" (SetFocus $ ItemFocus idx $ I.RuleFocus $ R.GoalFocus pth) [typicon "location"]
-                     else 
-                        button "button-icon button-icon-blue button-icon-goal" (SetFocus $ ItemFocus idx $ I.RuleFocus $ R.GoalFocus pth) [typicon "location-outline"]
+    metabinder' pth i n = case selected of
+      Just (R.ProofBinderFocus pth' i', n) | pth == pth', i == i' -> [metabinderEditor pth i n]
+      _ -> [button "editable editable-math" (SetFocus $ ItemFocus idx $ I.RuleFocus $ R.ProofBinderFocus pth i) [metabinder n]]
+
+    metabinderEditor pth i n =
+      form_
+        [class_ "editor editor-expanding", onSubmit (ItemAction (Just idx) $ I.RuleAct $ R.RenameProofBinder pth i)]
+        [ expandingTextbox "editor-textbox" UpdateInput n
+        , submitButton "button-icon button-icon-blue" [typicon "tick-outline"]
+        , button "button-icon button-icon-grey" Reset [typicon "times-outline"]
+        , focusHack "editor-textbox"
+        ]
+
+    goalButton pth =
+      if Just (R.GoalFocus pth) == fmap fst selected
+      then focusedButton "button-icon button-icon-active button-icon-goal" (SetFocus $ ItemFocus idx $ I.RuleFocus $ R.GoalFocus pth) [typicon "location"]
+      else button "button-icon button-icon-blue button-icon-goal" (SetFocus $ ItemFocus idx $ I.RuleFocus $ R.GoalFocus pth) [typicon "location-outline"]
--- View/Term.hs
+++ View/Term.hs
@@ -7,46 +7,49 @@ import View.Utils
 import Miso hiding (on)
 import Data.List (intersperse, dropWhileEnd, groupBy)
 
-renderTerm = renderTermCtx [] 
+renderTerm = renderTermCtx []
+
 renderTermCtx context opts trm = renderTerm' True context trm
   where
-    renderTerm' outer ctx (Lam (M v) t) = binder v (renderTerm' True (v:ctx) t)
-    renderTerm' outer ctx other = renderTerm'' outer ctx other 
-
-    renderTerm'' outer ctx t 
+    renderTerm' outer ctx (Lam (M v) t) = binder v (renderTerm' True (v : ctx) t)
+    renderTerm' outer ctx other = renderTerm'' outer ctx other
+    renderTerm'' outer ctx t
       | Lam _ _ <- t = multi $ parenthesise [renderTerm' False ctx t]
-      | (x, ts, []) <- peelApTelescope' t = case x of 
-           LocalVar j 
-              | j >= length ctx -> boundName (show j)
-              | length ctx - j <= length context -> freevar (ctx !! j)
-              | otherwise -> boundName (ctx !! j)
-           MetaVar i -> metavar i (renderITelescope ts)
-           Const s -> constant s
-              | (Const n, [], args) <- peelApTelescope' t
-              , showInfixes opts
-              , length (filter (== '_') n) == length args = multi $ (if outer then id else parenthesise) $ intersperse space (infixTerms n args)
-      | (x, ts, args) <- peelApTelescope' t = 
-              multi $ (if outer then id else parenthesise) 
-                    $ renderTerm'' False ctx x : renderITelescope ts ++ space : intersperse space (map (renderTerm'' False ctx) args)
-      where 
+      | (x, ts, []) <- peelApTelescope' t = case x of
+        LocalVar j
+          | j >= length ctx -> boundName (show j)
+          | length ctx - j <= length context -> freevar (ctx !! j)
+          | otherwise -> boundName (ctx !! j)
+        MetaVar i -> metavar i
+        Const s -> constant s
+
+      | (Const n, [], args) <- peelApTelescope' t
+      , showInfixes opts
+      , length (filter (== '_') n) == length args
+      = multi $ (if outer then id else parenthesise) $ intersperse space (infixTerms n args)
+
+      | (x, ts, args) <- peelApTelescope' t
+      = multi $ (if outer then id else parenthesise) $
+          renderTerm'' False ctx x : renderITelescope ts ++ space : intersperse space (map (renderTerm'' False ctx) args)
+      where
         infixTerms [] [] = []
         infixTerms str [] = [constant str]
-        infixTerms ('_':str) (x:xs) = renderTerm' False ctx x : infixTerms str xs 
-        infixTerms str args | (first, rest) <- span (/='_') str = constant first : infixTerms rest args
+        infixTerms ('_' : str) (x : xs) = renderTerm' False ctx x : infixTerms str xs
+        infixTerms str args | (first, rest) <- span (/= '_') str = constant first : infixTerms rest args
 
     freevar v = inline "term-freevar" (name v)
-    metavar v r = inline "term-metavar" (name ('?':v) ++ r)
+    metavar v = inline "term-metavar" (name ('?' : v))
     constant v = inline "term-const" (name v)
     boundName txt = inline "term-bound" (name txt)
     binder txt bdy = inline "term-binder" $ [boundName txt, text ".", space, bdy]
 
-    renderITelescope ts = []
-
-    peelApTelescope' t | (t', args) <- peelApTelescope t 
-                       = case t' of 
-                           MetaVar i | not (showTeles opts)
-                                     -> let (args1, args2) = span isAtom args
-                                         in (MetaVar i, args1, args2)
-                           _ -> (t',[],args)
-      where isAtom (LocalVar _) = True
-            isAtom _ = False
+    peelApTelescope' t | (t', args) <- peelApTelescope t =
+      case t' of
+        MetaVar i
+          | not (showTeles opts) ->
+            let (args1, args2) = span isAtom args
+             in (MetaVar i, args1, args2)
+        _ -> (t', [], args)
+      where
+        isAtom (LocalVar _) = True
+        isAtom _ = False
