98bd6653d8b8eb232a62f2940cdcfbf18174baca
Liam O'Connor <me@liamoc.net>
2021-10-18 19:57:43 +0100

    fix bug in mixfix parser, remove flag and old parser

--- StringRep.hs
+++ StringRep.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, RecursiveDo, CPP #-}
+{-# LANGUAGE OverloadedStrings, RecursiveDo #-}
 module StringRep (prettyPrint, parse) where
 
 import Data.Char
@@ -86,6 +86,7 @@ printHoley origin operends = go origin origin operends
 
 prettyPrint :: [Name] -> Term -> MS.MisoString 
 prettyPrint ctx t = MS.ms $ B.toLazyText $ go ctx t
+
 go :: [Name] -> Term -> B.Builder 
 go ctx (Lam (M x) t) = B.fromText (MS.fromMisoString x) <> ". " <> go (x:ctx) t
 go ctx e = go' ctx e
@@ -93,9 +94,10 @@ go' ctx (Ap a1 a2)
   | (x, ts) <- peelApTelescope (Ap a1 a2) = case x of 
     Const op 
       | isMixfix op, MS.count "_" op == length ts -> printHoley op $ (map $ go'' ctx) ts
-      | otherwise -> go' ctx a1 <> " " <> go'' ctx a2
-        where
-          isMixfix op = elem op [MS.toMisoString ys | xs <- mixfixDef, (ys, _) <- xs]  
+    _ -> go' ctx a1 <> " " <> go'' ctx a2
+  where
+    isMixfix op = elem op [MS.toMisoString ys | xs <- mixfixDef, (ys, _) <- xs]  
+    
 go' ctx (Lam n t) = "(" <> go ctx (Lam n t) <> ")"
 go' ctx e = go'' ctx e
 go'' ctx (LocalVar v) = B.fromText (MS.fromMisoString (ctx !! v))
@@ -124,62 +126,7 @@ unlex Dot = "."
 unlex (Binder s) = s <> "."
 unlex (Word s) = s
 
-
 type Parser a = ExceptT MS.MisoString (State [Token]) a
-
-runParser = runState . runExceptT
-
-expect :: Token -> Parser ()
-expect t = do
-  w <- lex
-  if w == t then pure () else parseError $ "Expected '" <> unlex t <> "', got '" <> unlex w <> "'."
-
-lex :: Parser Token
-lex = do
-  xs' <- get
-  when (null xs') (parseError $ "Unexpected end of input")
-  let (x:xs) = xs'
-  put xs
-  pure x
-
-peek :: Parser (Maybe Token)
-peek = do
-  xs <- get
-  pure $ listToMaybe xs
-
-parseError :: MS.MisoString -> Parser a
-parseError str = throwError str
-parser :: [Name] -> Parser Term
-parser ctx = do
-      w <- peek
-      case w of
-        Just (Binder w) -> lex >> (Lam (M w) <$> parser (w:ctx))
-        _ -> parser' ctx
-
-parser' :: [Name] -> Parser Term
-parser' ctx = toApplications <$> some (parser'' ctx)
-  where
-    toApplications [] = error "impossible"
-    toApplications [x] = x
-    toApplications [x,y] = Ap x y
-    toApplications (x:y:ys) = toApplications (Ap x y:ys)
-
-
-parser'' :: [Name] -> Parser Term
-parser'' ctx = do
-      w <- peek
-      case w of Just (Word w) -> lex >> pure (symbol w)
-                Just (LParen) -> do lex; x <- parser ctx; expect RParen;  pure x
-                Just x        -> parseError ("Unexpected '" <> unlex x <> "'")
-                Nothing       -> parseError ("Unexpected end of input")
-  where symbol w | Just v <- elemIndex w ctx = LocalVar v
-                 | otherwise = Const w
-
-fromSexps :: [Name] -> MS.MisoString -> Either Error Term
-fromSexps ctx s = case runParser (parser ctx) . preprocess . lexer $ s of
-              (Left e,_) -> Left e
-              (Right v,[]) -> Right v
-              (_,s) -> Left $ "Parse error: Leftover '" <> MS.concat (map unlex s) <> "'"
 
 fromMixfix :: [Name] -> MS.MisoString -> Either Error Term
 fromMixfix ctx s = case EP.fullParses (EP.parser grammar) $ preprocess . lexer $ s of
@@ -188,8 +135,4 @@ fromMixfix ctx s = case EP.fullParses (EP.parser grammar) $ preprocess . lexer $
                    ((x:xs), rep) -> Left $ "Parse error: ambiguous parsing result: " <> MS.concat (map ((prettyPrint ctx). (postProc ctx)) (x:xs))
 
 parse :: [Name] -> MS.MisoString -> Either Error Term
-#ifdef MIXFIX
 parse = fromMixfix
-#else
-parse = fromSexps
-#endif
