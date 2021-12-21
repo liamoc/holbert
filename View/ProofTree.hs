d0252a06a83686a18eb443b25793ff720e977a98
Rayhana <s1610748@sms.ed.ac.uk>
2021-12-21 16:27:00 +0000

    Update ProofTree.hs

--- View/ProofTree.hs
+++ View/ProofTree.hs
@@ -8,6 +8,7 @@ import DisplayOptions
 import qualified Item as I
 import qualified Rule as R
 import qualified Prop as P
+import qualified Terms as T
 import ProofTree
 import View.Prop
 import View.Utils
@@ -125,3 +126,32 @@ flatten (PT opts sks lcls g (Just (P.Transitivity, [a,b]))) = concatMap flatten 
 --      _ -> concatMap flatten [a,b]
 flatten (PT opts sks lcls g mgs) = [(PT opts sks lcls g Nothing)]
 
+extract :: ProofTree -> T.Term
+extract (PT opts sks lcls g mgs) = g
+
+renderTransitive :: [T.Term] -> [T.Term]
+renderTransitive (x:xs) = takeBoth x ++ concatMap takeSecond xs where
+  takeBoth :: T.Term -> [T.Term]
+  takeBoth (T.Ap (T.Const "_=_") (T.Ap a b)) = [a,b]
+  takeBoth _ = []
+
+  takeSecond :: T.Term -> [T.Term]
+  takeSecond (T.Ap (T.Const "_=_") (T.Ap a b)) = [b]
+  takeSecond _ = []
+
+-- PT Nothing [] [] (Ap (Const "_=_") (Ap (Const "A") (Const "E"))) (Just (Transitivity,[PT Nothing [] [] (Ap (Const "_=_") (Ap (Const "A") (Const "C"))) (Just (Transitivity,[PT Nothing [] [] (Ap (Const "_=_") (Ap (Const "A") (Const "B"))) Nothing,PT Nothing [] [] (Ap (Const "_=_") (Ap (Const "B") (Const "C"))) Nothing])),PT Nothing [] [] (Ap (Const "_=_") (Ap (Const "C") (Const "E"))) (Just (Transitivity,[PT Nothing [] [] (Ap (Const "_=_") (Ap (Const "C") (Const "D"))) Nothing,PT Nothing [] [] (Ap (Const "_=_") (Ap (Const "D") (Const "E"))) Nothing]))]))
+
+-- a = (T.Ap (T.Const (MS.toMisoString "_=_")) (T.Ap (T.Const (MS.toMisoString "A")) (T.Const (MS.toMisoString "B"))))
+-- b = (T.Ap (T.Const (MS.toMisoString "_=_")) (T.Ap (T.Const (MS.toMisoString "B")) (T.Const (MS.toMisoString "C"))))
+-- c = (T.Ap (T.Const (MS.toMisoString "_=_")) (T.Ap (T.Const (MS.toMisoString "C")) (T.Const (MS.toMisoString "D"))))
+-- d = (T.Ap (T.Const (MS.toMisoString "_=_")) (T.Ap (T.Const (MS.toMisoString "D")) (T.Const (MS.toMisoString "E"))))
+-- ab = (T.Ap (T.Const (MS.toMisoString "_=_")) (T.Ap (T.Const (MS.toMisoString "A")) (T.Const (MS.toMisoString "C"))))
+-- cd = (T.Ap (T.Const (MS.toMisoString "_=_")) (T.Ap (T.Const (MS.toMisoString "C")) (T.Const (MS.toMisoString "E"))))
+-- abcd = (T.Ap (T.Const (MS.toMisoString "_=_")) (T.Ap (T.Const (MS.toMisoString "A")) (T.Const (MS.toMisoString "E"))))
+-- pta = (PT Nothing [] [] a Nothing)
+-- ptb = (PT Nothing [] [] b Nothing)
+-- ptc = (PT Nothing [] [] c Nothing)
+-- ptd = (PT Nothing [] [] d Nothing)
+-- ptab = (PT Nothing [] [] ab (Just (P.Transitivity, [pta, ptb])))
+-- ptcd = (PT Nothing [] [] cd (Just (P.Transitivity, [ptc, ptd])))
+-- ptabcd = (PT Nothing [] [] abcd (Just (P.Transitivity, [ptab, ptcd])))
